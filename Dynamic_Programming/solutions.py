d5b34374b4d0a3501433e89983e31d45ca939938
Isaac <itrussell15@gmail.com>
2025-05-14 17:43:14 +0200

    Fix gitignore

---
+++ Dynamic_Programming/solutions.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def fibbonacci(n: int) -> int:
+    return 0
+
+def lis(x: List[int]) -> int:
+    return 0
+
+def knapsack(weights: List[int], values: List[int]) -> int:
+    return 0
