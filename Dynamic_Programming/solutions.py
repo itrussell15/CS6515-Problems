32389116d2b4d20bc35bfcffcca7e9c46f8b701a
Isaac <itrussell15@gmail.com>
2025-05-15 14:04:20 +0200

    Add docstrings

--- Dynamic_Programming/solutions.py
+++ Dynamic_Programming/solutions.py
@@ -1,13 +1,37 @@
 from typing import List
 
 def fibbonacci(n: int) -> int:
-    return 0
+    """
+    Fibonacci Numbers -> [0, 1, 1, 2, 3, 5, 8, 13, ...]
+        Runtime: O(?)
+        Memory:  O(?)
+    https://leetcode.com/problems/fibonacci-number/description/
+    """
+    return None
 
 def lis(x: List[int]) -> int:
-    return 0
+    """
+    Longest Increasing Subsequence
+        Runtime: O(?)
+        Memory:  O(?)
+    https://leetcode.com/problems/longest-increasing-subsequence/description/
+    """
+    return None
 
 def lcs(x: str, y: str) -> int:
-    return 0
+    """
+    Longest Common Subsequence
+        Runtime: O(?)
+        Memory:  O(?)
+    https://leetcode.com/problems/longest-common-subsequence/
+    """
+    return None
 
 def knapsack(weights: List[int], values: List[int]) -> int:
-    return 0
+    """
+    Knapsack Problem / Painting the Walls
+        Runtime: O(?)
+        Memory:  O(?)
+    https://leetcode.com/problems/painting-the-walls/description/
+    """
+    return None
